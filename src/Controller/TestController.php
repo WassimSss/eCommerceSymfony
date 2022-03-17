b07b374dcd82a4cfbe26b2beb27c932550810a5d
unknown <tojiikan@gmail.com>
2022-03-17 10:26:55 +0100

    Découverte de la notion de container

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -2,28 +2,42 @@
 
 namespace App\Controller;
 
+use App\Taxes\Calculator;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class TestController
 {
-    public function index() 
+    protected $calculator;
+
+    public function __construct(Calculator $calculator)
     {
+        $this->calculator = $calculator;
+    }
+
+    /**
+     * @Route("/", name="index")
+     */
+    public function index()
+    {
+        $tva = $this->calculator->calcul(100);
+        dump($tva);
         dd("okok");
     }
 
 
     /**
-     * @Route("/test/{age<\d+>?0}", name="test" methods={"GET", "POST"}, schemes={"http", "https"})
+     * @Route("/test/{age<\d+>?0}", name="test", methods={"GET", "POST"}, schemes={"http", "https"})
      */
-    public function test(Request $request, $age) 
+    public function test(Request $request, $age)
     {
         dump($request);
         // $age = $request->attributes->get('age');
 
         return new Response("Vous avez $age ans !");
-
-        
     }
+
+
+
 }
